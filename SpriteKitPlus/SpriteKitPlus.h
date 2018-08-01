0119850068d2a6c15b35107ee8140839a730b4b2
Lin Knudsen <lineknudsen@lins-macbook-air.local>
2018-07-31 21:16:26 -0700

    Initial Commit

---
+++ SpriteKitPlus/SpriteKitPlus.h
@@ -0,0 +1,19 @@
+//
+//  SpriteKitPlus.h
+//  SpriteKitPlus
+//
+//  Created by Lin Knudsen on 7/31/18.
+//  Copyright © 2018 Lin Knudsen. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SpriteKitPlus.
+FOUNDATION_EXPORT double SpriteKitPlusVersionNumber;
+
+//! Project version string for SpriteKitPlus.
+FOUNDATION_EXPORT const unsigned char SpriteKitPlusVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SpriteKitPlus/PublicHeader.h>
+
+
