84836448d1ef43b280ffbe5fb342d7e432313fec
Lin Knudsen <onlineknudsen@gmail.com>
2019-02-11 10:19:58 -0800

    Add GameScene template and Updatable protocol

---
+++ SpriteKitPlus/GameScene.swift
@@ -0,0 +1,81 @@
+//
+//  GameScene.swift
+//  SpriteKitPlus
+//
+//  Created by Lin Knudsen on 11/3/18.
+//  Copyright © 2018 Lin Knudsen. All rights reserved.
+//
+
+import SpriteKit
+
+open class GameScene: SKScene {
+    
+    public let inputManager: InputManager = InputManager()
+    
+    public var deltaTime: TimeInterval = 0.0
+    private var lastTime: TimeInterval = 0.0
+    
+    open override func didMove(to view: SKView) {
+        setupTrackingArea()
+        addChild(inputManager)
+    }
+    
+    required public init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    //MARK: - Update
+    open override func update(_ currentTime: TimeInterval) {
+        deltaTime = currentTime - lastTime
+        lastTime = currentTime
+        
+        children.forEach { ($0 as? Updatable)?.update(deltaTime: deltaTime) }
+    }
+    
+    open override func didFinishUpdate() {
+        children.forEach { ($0 as? LateUpdatable)?.lateUpdate() }
+    }
+    
+    open override func willMove(from view: SKView) {
+        view.trackingAreas.forEach { view.removeTrackingArea($0)}
+    }
+    
+    private func setupTrackingArea() {
+        guard let view = view else { return }
+        
+        let options = [NSTrackingArea.Options.mouseMoved, .activeInKeyWindow] as NSTrackingArea.Options
+        let trackingArea = NSTrackingArea(rect: view.frame, options: options, owner: self, userInfo: nil)
+        
+        view.addTrackingArea(trackingArea)
+    }
+
+}
+//MARK: - Input
+extension GameScene {
+    open override func keyDown(with event: NSEvent) {
+        inputManager.setKeyDown(keyCode: event.keyCode)
+    }
+    
+    open override func keyUp(with event: NSEvent) {
+        inputManager.setKeyDown(keyCode: event.keyCode)
+    }
+    
+    open override func mouseDown(with event: NSEvent) {
+        inputManager.setMouseDown(mouseButton: Mouse.leftButton)
+    }
+    
+    open override func mouseUp(with event: NSEvent) {
+        inputManager.setMouseUp(mouseButton: Mouse.leftButton)
+    }
+    
+    open override func rightMouseDown(with event: NSEvent) {
+        inputManager.setMouseDown(mouseButton: Mouse.rightButton)
+    }
+    
+    open override func rightMouseUp(with event: NSEvent) {
+        inputManager.setMouseUp(mouseButton: Mouse.rightButton)
+    }
+    
+    open override func mouseMoved(with event: NSEvent) {
+        inputManager.setMousePosition(event.location(in: self))
+    }
+}
--- SpriteKitPlus/Protocols.swift
+++ SpriteKitPlus/Protocols.swift
@@ -6,8 +6,9 @@
 //  Copyright © 2018 Lin Knudsen. All rights reserved.
 //
 
+///Conform this protocol if you want your object to be called in the game loop when you add it as a child in the scene
 public protocol Updatable {
-    func update ()
+    func update (deltaTime: TimeInterval)
 }
 
 public protocol LateUpdatable {
